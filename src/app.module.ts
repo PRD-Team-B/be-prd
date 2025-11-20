2891c679487b57fb9580622fd23b18a693d733a9
Salman Althof <salmanalthof31@gmail.com>
2025-11-21 00:40:15 +0700

    feat: create loger middleware for debugging and tracking

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './global/middlewares/logger.middleware';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
