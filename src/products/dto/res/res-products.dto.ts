5fcde0a0d9d54aa8d925d2032e4ebd7cd2c070c3
Salman Althof <salmanalthof31@gmail.com>
2025-11-20 23:51:55 +0700

    feat: create response body dto

---
+++ src/products/dto/res/res-products.dto.ts
@@ -0,0 +1,25 @@
+import { Expose, Type } from "class-transformer";
+
+
+export class ProductBodyDto {
+    @Expose()
+    @Type(() => Number)
+    id: number
+    @Expose()
+    @Type(() => String)
+    title: string
+    @Expose()
+    @Type(() => String)
+    photo: string
+    @Expose()
+    @Type(() => Number)
+    quantity: number
+    @Expose()
+    @Type(() => Date)
+    created_at: Date
+    @Expose()
+    @Type(() => Date)
+    updated_at: Date
+    @Expose()
+    reviews
+}
