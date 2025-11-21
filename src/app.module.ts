4d78be97cb50f3eea4ea1bc41cab3ffb2e8bc402
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2025-11-21 20:32:15 +0700

    refactor: change prisma module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './global/middlewares/logger.middleware';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [{ provide: 'IAppService', useClass: AppService }],
 })
