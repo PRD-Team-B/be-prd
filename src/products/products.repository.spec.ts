9a993dd4f96d6b91d769fb1df7427d51b6d54118
Salman Althof <salmanalthof31@gmail.com>
2025-11-24 23:33:38 +0700

    testing: testing get product

---
+++ src/products/products.repository.spec.ts
@@ -0,0 +1,88 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsRepository } from './products.repository';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ProductsRepositoryItf } from './products.repository.interface';
+
+describe('ProductsRepository - getProduct()', () => {
+    let repository: ProductsRepositoryItf;
+    let prismaMock: any;
+
+    const mockProduct = {
+        id: 1,
+        title: "Kebaya",
+        photo: "https://res.cloudinary.com/dgquproum/image/upload/v1763630448/kebaya-akad_fykjea.jpg",
+        quantity: 10,
+        created_at: "2022-08-01T08:10:00.000Z",
+        updated_at: "2022-08-01T08:10:00.000Z",
+        reviews: [
+            { 
+                id: 1,
+                products_id: 1, 
+                name: "Sarah", ratings: 5, 
+                comments: "Bagus", 
+                created_at: "2024-11-01T08:10:00.000Z" 
+            },
+            { 
+                id: 2, 
+                products_id: 1, 
+                name: "Yodha",
+                ratings: 5, 
+                comments: "Mantap", 
+                created_at: "2024-11-02T12:40:00.000Z" 
+            },
+        ]
+    };
+
+    const mockAvg = {
+        _avg: { ratings: 4.4 }
+    };
+
+    beforeEach(async () => {
+        prismaMock = {
+            products: {
+                findUnique: jest.fn(),
+            },
+            reviews: {
+                aggregate: jest.fn(),
+            },
+            $transaction: jest.fn(),
+        };
+
+        const module: TestingModule = await Test.createTestingModule({
+        providers: [
+            ProductsRepository,
+            { provide: PrismaService, useValue: prismaMock },
+        ],
+        }).compile();
+
+        repository = module.get<ProductsRepositoryItf>(ProductsRepository);
+    });
+
+    afterEach(() => jest.clearAllMocks());
+
+    test('should return product with average rating', async () => {
+        prismaMock.$transaction.mockResolvedValue([
+            mockProduct,
+            mockAvg
+        ]);
+
+        const result = await repository.getProduct(1);
+
+        expect(prismaMock.$transaction).toHaveBeenCalled();
+        expect(result).toEqual({
+            ...mockProduct,
+            averageRating: 4.4
+        });
+
+        expect(prismaMock.$transaction).toHaveBeenCalledWith([
+        prismaMock.products.findUnique({
+            where: { id: 1 },
+            include: { reviews: true }
+        }),
+        prismaMock.reviews.aggregate({
+            _avg: { ratings: true },
+            where: { products_id: 1 }
+        })
+        ]);
+    });
+});
