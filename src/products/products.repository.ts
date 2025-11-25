ffa446dc1c9b5148f7cb839bc80655196514532b
Salman Althof <salmanalthof31@gmail.com>
2025-11-25 18:56:36 +0700

    feat: implement custom exception database

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -4,27 +4,32 @@ import {
   OutDetailProduct,
   ProductsRepositoryItf,
 } from './products.repository.interface';
+import { handlePrismaError } from '../global/utils/prisma.error.util';
 
 @Injectable()
 export class ProductsRepository implements ProductsRepositoryItf {
   constructor(private readonly prisma: PrismaService) {}
 
   async getProduct(id: number): Promise<OutDetailProduct | null> {
-    const [product, avg] = await this.prisma.$transaction([
-      this.prisma.products.findUnique({
-        where: { id },
-        include: { reviews: true }
-      }),
-      this.prisma.reviews.aggregate({
-        _avg: { ratings: true },
-        where: { products_id: id },
-      })
-    ]);
+    try {
+      const [product, avg] = await this.prisma.$transaction([
+        this.prisma.products.findUnique({
+          where: { id },
+          include: { reviews: true }
+        }),
+        this.prisma.reviews.aggregate({
+          _avg: { ratings: true },
+          where: { products_id: id },
+        })
+      ]);
 
-    if(!product) return null
-    return {
-      ...product,
-      averageRating: avg._avg.ratings ?? 0
+      if(!product) return null
+      return {
+        ...product,
+        averageRating: avg._avg.ratings ?? 0
+      }
+    } catch (error) {
+      handlePrismaError(error)
     }
   }
 }
