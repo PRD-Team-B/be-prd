716820bc19e2b50ebef99be8f44826f728b8720c
Salman Althof <salmanalthof31@gmail.com>
2025-11-25 11:32:48 +0700

    feat: create custom exception for handle prisma error

---
+++ src/global/utils/prisma.error.util.ts
@@ -0,0 +1,26 @@
+import { Prisma } from "@prisma/client";
+import { DatabaseException } from "../exception/database-exception";
+
+
+export function handlePrismaError(error): never {
+    // Error unique constraint, foreign key (example P1001: prisma not connect to database)
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if(error.code === 'P2025') throw new DatabaseException('data in database not found')
+        throw new DatabaseException(`Database error: ${error.code} - ${error.message}`);
+    }
+    // problem connection DB
+    if (error instanceof Prisma.PrismaClientInitializationError) {
+        throw new DatabaseException(`Database connection failed: ${error.message}`);
+    }
+    // Query Prisma not valid
+    if (error instanceof Prisma.PrismaClientValidationError) {
+
+        throw new DatabaseException(`Invalid database query: ${error.message}`);
+    }
+    // PgBouncer / Postgres native error
+    if (error && typeof error === 'object' && 'code' in error) {
+        throw new DatabaseException(`Postgres error: ${error.code} - ${error.message}`);
+    }
+    // Fallback unknown error
+    throw new DatabaseException();
+}
