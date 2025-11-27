b875a0d0acf8d309bfca1a2463769cb2109e371d
Salman <salmanalthof31@gmail.com>
2025-11-27 15:51:00 +0700

    test: testing app.controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { IAppService } from './app.interface';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  const mockAppService: IAppService = {
+    getHealthCheck: jest.fn().mockResolvedValue('OK'),
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        { provide: 'IAppService', useValue: mockAppService, },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  test('should return health check status', async () => {
+    const result = await appController.getHello();
+
+    expect(result).toBe('OK');
+    expect(mockAppService.getHealthCheck).toHaveBeenCalled();
   });
 });
